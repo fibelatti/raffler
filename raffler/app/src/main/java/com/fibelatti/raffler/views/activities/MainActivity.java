a4d38ccb2311837159e516f17526229109b0bf19
Filipe Belatti <fibelatti@gmail.com>
2016-08-12 15:30:34 -0300

    Removing unused main menu

--- raffler/app/src/main/java/com/fibelatti/raffler/views/activities/MainActivity.java
+++ raffler/app/src/main/java/com/fibelatti/raffler/views/activities/MainActivity.java
@@ -7,8 +7,6 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -58,22 +56,6 @@ public class MainActivity extends BaseActivity {
         super.onResume();
         fetchData();
         setUpValues();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     private void setUpLayout() {
