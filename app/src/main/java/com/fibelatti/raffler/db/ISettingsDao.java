4e0b157f6506888120776481f94b36dfda3778f4
Filipe Belatti <fibelatti@gmail.com>
2016-08-13 15:50:01 -0300

    Creating database assets for settings

---
+++ app/src/main/java/com/fibelatti/raffler/db/ISettingsDao.java
@@ -0,0 +1,9 @@
+package com.fibelatti.raffler.db;
+
+/**
+ * Created by fibelatti on 13/08/16.
+ */
+public interface ISettingsDao {
+    boolean getRouletteMusicEnabled();
+    boolean setRouletteMusicEnabled(boolean value);
+}
