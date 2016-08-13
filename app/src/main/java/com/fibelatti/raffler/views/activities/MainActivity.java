a9a33b333d7794b9bde763eb8098cadea9a5a951
Filipe Belatti <fibelatti@gmail.com>
2016-08-13 14:27:40 -0300

    Including settings in main menu

--- app/src/main/java/com/fibelatti/raffler/views/activities/MainActivity.java
+++ app/src/main/java/com/fibelatti/raffler/views/activities/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.widget.TextView;
@@ -56,6 +58,23 @@ public class MainActivity extends BaseActivity {
         super.onResume();
         fetchData();
         setUpValues();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                startSettingsActivity();
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setUpLayout() {
@@ -120,6 +139,11 @@ public class MainActivity extends BaseActivity {
     private void startGroupFormActivity() {
         Intent intent = new Intent(this, GroupFormActivity.class);
         intent.putExtra(Constants.INTENT_EXTRA_GROUP, new Group());
+        startActivity(intent);
+    }
+
+    private void startSettingsActivity() {
+        Intent intent = new Intent(this, SettingsActivity.class);
         startActivity(intent);
     }
 }
