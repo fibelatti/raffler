782dcc2062724f145afedec9412d41be1ff5fcd4
Filipe Belatti <fibelatti@gmail.com>
2016-11-14 22:34:32 +0100

    Different Application file for debug and release

---
+++ app/src/debug/java/com/fibelatti/raffler/RafflerApplication.java
@@ -0,0 +1,45 @@
+package com.fibelatti.raffler;
+
+import android.app.Application;
+
+import com.crashlytics.android.Crashlytics;
+import com.crashlytics.android.core.CrashlyticsCore;
+import com.fibelatti.raffler.db.Database;
+import com.squareup.leakcanary.LeakCanary;
+
+import io.fabric.sdk.android.Fabric;
+
+public class RafflerApplication
+        extends Application {
+    public static final String TAG = RafflerApplication.class.getSimpleName();
+    public static RafflerApplication app;
+    public static Database db;
+
+    public RafflerApplication() {
+        super();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        app = this;
+
+        db = new Database(this);
+        db.open();
+
+        Fabric.with(this, new Crashlytics.Builder()
+                .core(new CrashlyticsCore.Builder()
+                        .disabled(Database.settingsDao.getCrashReportEnabled())
+                        .build()
+                ).build());
+
+        LeakCanary.install(this);
+    }
+
+    @Override
+    public void onTerminate() {
+        db.close();
+        super.onTerminate();
+    }
+}
